37e6dbc4fc5bb2bde5e6ce8148d5ec44cde698f9
Sho Ito <i.sho.0628@gmail.com>
2024-08-09 01:07:48 -0400

    feat: add a callback to notify the external app

--- app/models/edit_request.rb
+++ app/models/edit_request.rb
@@ -6,7 +6,17 @@ class EditRequest < ApplicationRecord # :nodoc:
 
   enum status: { pending: 0, approved: 1, rejected: 2 }
 
+  after_commit :notify_edit_request, on: :create
+
   def requested_fields
     edit_request_details.map(&:field_name)
+  end
+
+  private
+
+  def notify_edit_request
+    # In the real application, we would notify the external system via a webhook or some other mechanism
+    # in a background job. For the sake of simplicity, we will just call the method directly.
+    EditRequestNotifier.notify(self)
   end
 end
